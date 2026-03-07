3ff6d586a62f5328f50d4a2a69141a5232dc7165
Guo Xingchen <e1459295@u.nus.edu>
2026-03-07 23:55:49 +0800

    Complete implementation of Remark command and fix test files

--- src/main/java/seedu/address/model/person/Remark.java
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -1,5 +1,43 @@
 package seedu.address.model.person;
 
+import static java.util.Objects.requireNonNull;
+
+/**
+ * Represents a remark for a person.
+ */
 public class Remark {
+    public static final String VALIDATION_REGEX = "[^\\s].*";
+    public final String value;
+    /**
+     * Constructs an {@code Remark}.
+     *
+     * @param remark A valid remark.
+     */
+    public Remark(String remark) {
+        requireNonNull(remark);
+        value = remark;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this
+                || (other instanceof Remark
+                && value.equals(((Remark) other).value));
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+    public static boolean isValidRemark(String test) {
+        return test.matches(VALIDATION_REGEX);
+    }
+
 
 }
---
+++ src/test/java/seedu/address/logic/commands/RemarkCommandTest.java
@@ -0,0 +1,97 @@
+package seedu.address.logic.commands;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+import seedu.address.model.person.Person;
+import seedu.address.model.person.Remark;
+import seedu.address.logic.Messages;
+
+
+public class RemarkCommandTest {
+
+    public static final String VALID_REMARK_AMY = "Like skiing.";
+    public static final String VALID_REMARK_BOB = "Favourite pastime: Eating";
+
+    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+
+//    @Test
+//    public void execute() {
+//        final Remark remark = new Remark("Some remark");
+//
+//        assertCommandFailure(new RemarkCommand(INDEX_FIRST_PERSON, remark), model,
+//                String.format(MESSAGE_ARGUMENTS, INDEX_FIRST_PERSON.getOneBased(), remark));
+//    }
+
+    @Test
+    public void equals() {
+        final RemarkCommand standardCommand = new RemarkCommand(INDEX_FIRST_PERSON, new Remark(VALID_REMARK_AMY));
+
+        // same values -> returns true
+        RemarkCommand commandWithSameValues = new RemarkCommand(INDEX_FIRST_PERSON, new Remark(VALID_REMARK_AMY));
+        assertTrue(standardCommand.equals(commandWithSameValues));
+
+        // same object -> returns true
+        assertTrue(standardCommand.equals(standardCommand));
+
+        // null -> returns false
+        assertFalse(standardCommand.equals(null));
+
+        // different types -> returns false
+        assertFalse(standardCommand.equals(new ClearCommand()));
+
+        // different index -> returns false
+        assertFalse(standardCommand.equals(new RemarkCommand(INDEX_SECOND_PERSON, new Remark(VALID_REMARK_AMY))));
+
+        // different remark -> returns false
+        assertFalse(standardCommand.equals(new RemarkCommand(INDEX_FIRST_PERSON, new Remark(VALID_REMARK_BOB))));
+    }
+
+    @Test
+    public void execute_addRemark_success() {
+        Person personToEdit = model.getFilteredPersonList().get(0);
+
+        Remark remark = new Remark("friend");
+
+        RemarkCommand remarkCommand = new RemarkCommand(INDEX_FIRST_PERSON, remark);
+
+        String expectedMessage = String.format(
+                RemarkCommand.MESSAGE_ADD_REMARK_SUCCESS,
+                Messages.format(personToEdit));
+
+        // 创建编辑后的 person
+        Person editedPerson = new Person(
+                personToEdit.getName(),
+                personToEdit.getPhone(),
+                personToEdit.getEmail(),
+                personToEdit.getAddress(),
+                remark,
+                personToEdit.getTags());
+
+        Model expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
+        expectedModel.setPerson(personToEdit, editedPerson);
+
+        assertCommandSuccess(remarkCommand, model, expectedMessage, expectedModel);
+    }
+
+    @Test
+    public void execute_invalidPersonIndex_throwsCommandException() {
+        Index outOfBoundIndex = Index.fromOneBased(model.getFilteredPersonList().size() + 1);
+
+        RemarkCommand remarkCommand = new RemarkCommand(outOfBoundIndex, new Remark("test"));
+
+        assertCommandFailure(remarkCommand, model, Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+    }
+}
+
